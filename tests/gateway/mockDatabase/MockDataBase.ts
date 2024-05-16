0f375477a999bf4712bdb19cb55fdbda92bae086
Bruno Blauzius Schuindt <bruno.blauzius@lojacorr.local>
2024-05-16 09:00:12 -0300

    testes implementados, repository e entity

---
+++ tests/gateway/mockDatabase/MockDataBase.ts
@@ -0,0 +1,16 @@
+import { IDataBase } from '../../../interfaces/IDataBase';
+import { jest } from '@jest/globals';
+
+// Defina um objeto com métodos mockados para a interface IDataBase
+const mockDataBase: jest.Mocked<IDataBase> = {
+    store: jest.fn().mockReturnValue({ insertId: 1 }) ,
+    update: jest.fn().mockReturnValue(undefined),
+    delete: jest.fn().mockReturnValue(undefined),
+    find: jest.fn().mockReturnValue([
+        { id: 1, idPedido: 33262665 },
+        { id: 2, idPedido: 32226599 }
+    ]),
+    query : jest.fn().mockReturnValue(undefined)
+};
+
+export default mockDataBase;
