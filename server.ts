c73f383950af06bca626e9cbf054a474b5ab04da
bruno-blauzius <147417627+bruno-blauzius@users.noreply.github.com>
2024-05-18 23:36:56 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ export default class Server{
 
         this.app.route('/').get((req, res) => {
             res.json({
-                'version' : '1.1.7',
+                'version' : '1.1.8',
                 'date' : '2024-05-18' 
             });
         });
